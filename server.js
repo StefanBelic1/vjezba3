86b67e4ccd0e8e7636f3653ed49492b3d7892a99
Stefan Belić <127685759+StefanBelic1@users.noreply.github.com>
2025-04-10 15:34:24 +0200

    ejs galerija

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
-app.use(express.static('public')); // "posluzuje" index.html
-// Automatski koristi sve iz mape public
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
-res.send("Ili obican tekst ako nema HTML datoteke.");
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+
+app.get('/slike', (req, res) => {
+  const dataPath = path.join(__dirname, 'images.json');
+  const images = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+  res.render('slike', { images });
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
